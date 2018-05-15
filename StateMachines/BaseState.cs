fe9a65341e0189383a99faf2fc911e6d1e1788b2
AdamH <>
2018-05-15 21:52:21 +0200

    Unneeded public method changed to protected

--- StateMachines/BaseState.cs
+++ StateMachines/BaseState.cs
@@ -3,6 +3,7 @@
 // Interface of a machine's state.
 //
 // 22-Jan-2018   Created.
+// 15-May-2018   A method for retrieving state ID added.
 //
 //////////////////////////////////////////////////////////////
 
@@ -18,6 +19,19 @@ public abstract class BaseState
     public BaseState( BaseStateMachine parentStateMachine )
     {
         m_StateMachine = parentStateMachine;
+    }
+
+    /// <summary>Gets the ID of the state.</summary>
+    /// <returns>ID of the state.</returns>
+    abstract public StateId GetStateId();
+
+    /// <summary>ID of the states.</summary>
+    public enum StateId
+    {
+        CREATED_STATE,
+        PROCESSING_STATE,
+        FINISHED_STATE,
+        STATES_COUNT
     }
 
     /// <summary>A state machine that owns the state.</summary>
