9dbe3cf506db5d1b8e5b182a17f4a19a1ec5925f
AdamH <>
2018-05-15 20:29:18 +0200

    State added to execute operation after processing the input file

---
+++ StateMachines/InputFile/FinishedState.cs
@@ -0,0 +1,30 @@
+﻿//////////////////////////////////////////////////////////////
+//
+// Created state of an input file state machine.
+//
+// 15-May-2018   Created.
+//
+//////////////////////////////////////////////////////////////
+
+namespace NeoDesk
+{
+namespace StateMachines
+{
+namespace InputFile
+{
+
+public class FinishedState : InputFileBaseState
+{
+
+    /// <summary>Constructor.</summary>
+    /// <param name="parentStateMachine">A state machine to own the state.</param>
+    public FinishedState( BaseStateMachine parentStateMachine ) : base( parentStateMachine )
+    {
+        // Nothing to do.
+    }
+
+}
+
+} // namespace InputFile
+} // namespace StateMachines
+} // namespace NeoDesk
