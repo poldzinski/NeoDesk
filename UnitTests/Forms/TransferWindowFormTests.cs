cf0cae366bcf0e1dad8cd1b7f1f825213e2ad602
AdamH <>
2018-05-16 21:12:16 +0200

    Creation of unit tests within TransferWindowForm

--- UnitTests/Forms/TransferWindowFormTests.cs
+++ UnitTests/Forms/TransferWindowFormTests.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//////////////////////////////////////////////////////////////
+//
+// Unit tests within TransferWindowForm.
+//
+// 15-May-2018   Created.
+//
+//////////////////////////////////////////////////////////////
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Windows.Forms;
 
 using NeoDesk.Forms.TransferWindow;
@@ -12,11 +20,22 @@ namespace Forms
 public class TransferWindowFormTests
 {
     [TestMethod()]
-    public void TransferWindowFormCreation()
+    public void TransferWindowFormHappyPath()
     {
+        string testFileName = "Forms/TestInputFile.txt";
         TransferWindowForm transferWindowForm = new TransferWindowForm();
 
         Assert.IsNotNull( transferWindowForm );
+        transferWindowForm.ProcessFile( testFileName );
+    }
+
+    [TestMethod()]
+    [ExpectedException( typeof( System.IO.FileNotFoundException ) )]
+    public void TransferWindowFormWrongFileName()
+    {
+        const string testFileName = "WrongInputFile.txt";
+        TransferWindowForm transferWindowForm = new TransferWindowForm();
+        transferWindowForm.ProcessFile( testFileName );
     }
 }
 
