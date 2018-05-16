f1d9b899b4d492c6c6f49068551bca2b776d636d
AdamH <>
2018-05-16 23:04:36 +0200

    Initial version

---
+++ Menus/MenuItems/OptionsMenuItem.cs
@@ -0,0 +1,33 @@
+﻿//////////////////////////////////////////////////////////////
+//
+// Implementation of an options menu item.
+//
+// 16-May-2018   Created.
+//
+//////////////////////////////////////////////////////////////
+
+using System;
+using System.Windows.Forms;
+
+namespace NeoDesk
+{
+namespace Menus
+{
+namespace MenuItems
+{
+
+class OptionsMenuItem : BaseMenuItem
+{
+    /// <summary>Constructor.</summary>
+    public OptionsMenuItem() : base( m_Description )
+    {
+        // Nothing to do.
+    }
+                
+    /// <summary>Description.</summary>
+    private const string m_Description = "&Options...";
+}
+
+} // MenuItems
+} // Menus
+} // NeoDesk
