327e9812e9bc35d7fec43beb06c4e4c5e8749106
AdamH <>
2018-05-16 23:05:00 +0200

    Initial version

---
+++ Menus/Submenus/ToolsSubmenu.cs
@@ -0,0 +1,33 @@
+﻿//////////////////////////////////////////////////////////////
+//
+// Implementation of the tools submenu.
+//
+// 16-May-2018   Created.
+//
+//////////////////////////////////////////////////////////////
+
+using System;
+using System.Windows.Forms;
+
+namespace NeoDesk
+{
+namespace Menus
+{
+namespace Submenus
+{
+
+class ToolsSubmenu : BaseSubmenu
+{
+    /// <summary>Constructor.</summary>
+    public ToolsSubmenu() : base( m_Description )
+    {
+        // Nothing to do.
+    }
+                
+    /// <summary>Description.</summary>
+    private const string m_Description = "&Tools";
+}
+
+} // Submenus
+} // Menus
+} // NeoDesk
