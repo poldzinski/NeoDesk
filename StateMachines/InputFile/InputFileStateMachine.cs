df0c6634827eb61c6168eed37c0cf35ffec207f2
AdamH <>
2018-05-23 22:20:03 +0200

    Implementation removed

--- StateMachines/InputFile/InputFileStateMachine.cs
+++ StateMachines/InputFile/InputFileStateMachine.cs
@@ -4,10 +4,9 @@
 //
 // 22-Jan-2018   Created.
 // 15-May-2018   Processing and finished states added.
+// 23-May-2018   Implementation removed.
 //
 //////////////////////////////////////////////////////////////
-
-using System.Collections.Generic;
 
 namespace NeoDesk
 {
@@ -16,31 +15,19 @@ namespace StateMachines
 namespace InputFile
 {
 
-public class InputFileStateMachine : BaseStateMachine
+abstract public class InputFileStateMachine : BaseStateMachine
 {
     /// <summary>Constructor.</summary>
     /// <param name="inputFileName">Name of a file to be processed.</param>
     public InputFileStateMachine( string inputFileName )
     {
-        m_Created = new CreatedState( this );
-        m_Processing = new ProcessingState( this );
-        m_Finished = new FinishedState( this );
-
-        AddState( m_Created );
-        AddState( m_Processing );
-        AddState( m_Finished );
-
-        GoToState( m_Created );
-
         m_Lines = System.IO.File.ReadAllLines( inputFileName );
         m_CurrentLine = 0;
-
-        GoToState( m_Processing );
     }
 
     /// <summary>Processes next input value.</summary>
     /// <returns>Result of the operation.</returns>
-    public bool ProcessInput()
+    virtual public bool ProcessInput()
     {
         InputFileBaseState currentState = GetCurrentState() as InputFileBaseState;
 
@@ -48,8 +35,6 @@ public class InputFileStateMachine : BaseStateMachine
         {
             return currentState.ProcessInput( m_Lines[ m_CurrentLine++ ] );
         }
-
-        GoToState( m_Finished );
 
         return false;
     }
@@ -68,12 +53,6 @@ public class InputFileStateMachine : BaseStateMachine
         return m_CurrentLine;
     }
 
-    /// <summary>State after creating.</summary>    
-    private InputFileBaseState m_Created;
-    /// <summary>State while processing.</summary>    
-    private InputFileBaseState m_Processing;
-    /// <summary>State after finishing.</summary>    
-    private InputFileBaseState m_Finished;
     /// <summary>File's content.</summary>
     private string[] m_Lines;
     /// <summary>Current line to be processed.</summary>
