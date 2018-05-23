6a05c285e87eabccc86b46d4dc70a98052bfdf59
AdamH <>
2018-05-23 22:16:43 +0200

    Implementation added

--- Forms/TransferWindow/ProjectFileStateMachineImpl.cs
+++ Forms/TransferWindow/ProjectFileStateMachineImpl.cs
@@ -4,6 +4,7 @@
 //
 // 29-Mar-2018   Created.
 // 15-May-2018   File name parameter added to the constructor.
+// 23-May-2018   Implementation added.
 //
 //////////////////////////////////////////////////////////////
 
@@ -22,10 +23,42 @@ public class ProjectFileStateMachineImpl : InputFileStateMachine
 {
     /// <summary>Constructor.</summary>
     /// <param name="inputFileName">Name of a file to be processed.</param>
-    public ProjectFileStateMachineImpl( string inputFileName ) : base ( inputFileName )
+    public ProjectFileStateMachineImpl( string inputFileName ) : base( inputFileName )
     {
+        m_Created = new CreatedState( this );
+        m_Processing = new ProcessingState( this );
+        m_Finished = new FinishedState( this );
+
+        AddState( m_Created );
+        AddState( m_Processing );
+        AddState( m_Finished );
+
+        GoToState( m_Created );
     }
         
+    /// <summary>Processes next input value.</summary>
+    /// <returns>Result of the operation.</returns>
+    override public bool ProcessInput()
+    {
+        GoToState( m_Processing );
+
+        bool baseResult = base.ProcessInput();
+
+        if ( baseResult == false )
+        {    
+            GoToState( m_Finished );
+        }
+
+        return baseResult;
+    }
+
+    /// <summary>State after creating.</summary>    
+    private InputFileBaseState m_Created;
+    /// <summary>State while processing.</summary>    
+    private InputFileBaseState m_Processing;
+    /// <summary>State after finishing.</summary>    
+    private InputFileBaseState m_Finished;
+
 }
 
 } // namespace TransferWindow
